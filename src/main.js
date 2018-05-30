7ae2a52a3e6343edfa3f03105f047eec86814730
Farhan Abuhazan <farhan.abuhazan@gmail.com>
2018-05-30 11:19:04 +0700

    new version 2

--- src/main.js
+++
@@ -1,4 +0,0 @@
-var ws = require('./modules/ws/ws');
-var io = require('./modules/socket').setIO(ws.io);
-var store = require('./modules/store/store');
-var ser = require('./modules/serialport/serialport').Store(store);
--- src/modules/temp/activity.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    type: 'Lettuce',
-    dth: 33,
-    ptd: "2017-01-22"
-}
